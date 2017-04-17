50cebe314011d7f12f0dfb9acd0d1069693ae75e
Evan Hahn <me@evanhahn.com>
2017-04-16 21:29:03 -0500

    Uglify client-side JavaScript

--- tasks/app/build-assets/javascripts.ts
+++ tasks/app/build-assets/javascripts.ts
@@ -5,6 +5,8 @@ import * as gulp from 'gulp'
 import { rollup } from 'rollup'
 import * as rollupTypescript from 'rollup-plugin-typescript'
 import { join } from 'path'
+import * as fs from 'fs'
+import { minify } from 'uglify-js'
 
 const ROOT = join(__dirname, '..', '..', '..')
 const TS_CONFIG = join(ROOT, 'tsconfig.json')
@@ -12,18 +14,30 @@ const JAVASCRIPT_ASSET_MAIN_SRC = join(ROOT, 'app', 'assets', 'javascripts', 'in
 export const JAVASCRIPT_ASSET_FILES_SRC: string = join(ROOT, 'app', 'assets', 'javascripts', '**/*.ts')
 const JAVASCRIPT_ASSET_MAIN_DIST = join(ROOT, 'dist', 'app', 'assets', 'javascripts', 'index.js')
 
-gulp.task('app.build.assets.javascripts', function () {
-  return rollup({
+gulp.task('app.build.assets.javascripts', function (done: (err?: Error) => any) {
+  const isProduction: boolean = process.env.NODE_ENV === 'production'
+
+  rollup({
     entry: JAVASCRIPT_ASSET_MAIN_SRC,
     plugins: [rollupTypescript({
+      target: 'es5',
       typescript: require('typescript')
     })]
   })
     .then(function (bundle: any) {
-      bundle.write({
+      return bundle.write({
         format: 'iife',
         dest: JAVASCRIPT_ASSET_MAIN_DIST,
-        sourceMap: process.env.NODE_ENV !== 'production'
+        sourceMap: !isProduction
       })
     })
+    .then(function () {
+      if (isProduction) {
+        const minified: string = minify(JAVASCRIPT_ASSET_MAIN_DIST).code
+        fs.writeFile(JAVASCRIPT_ASSET_MAIN_DIST, minified, done)
+      } else {
+        done()
+      }
+    })
+    .catch(done)
 })
